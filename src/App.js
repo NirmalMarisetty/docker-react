e9813237bf78eaaa002effbf63150aa75d2daa47
Nirmal Marisetty <nirmal@Nirmals-Air.hsd1.ca.comcast.net>
2018-12-24 08:07:27 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,19 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <h1 className="App-title">Welcome to React </h1>
+          </header>
+          <p className="App-intro">
+            Bye.. for now!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          </div>
     );
   }
 }
